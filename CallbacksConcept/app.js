7b31aee6a867ab4e145f165b3854c661f7936891
HOANGHUNG <hung.nguyenhoang@apps-cyclone.com>
2018-10-10 20:06:25 +0700

    Callbacks Concept

---
+++ CallbacksConcept/app.js
@@ -0,0 +1,14 @@
+var fs = require('fs');
+
+// var data = fs.readFileSync('input.txt');
+
+fs.readFile('input.txt',function (err, data) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data.toString());
+    }
+});
+
+// console.log(data.toString());
+console.log("Program end");
