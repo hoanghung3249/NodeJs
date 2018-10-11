1822523c0011fe4f8440073cf7f8dea0035722a3
HOANGHUNG <hung.nguyenhoang@apps-cyclone.com>
2018-10-11 19:57:24 +0700

    Express

---
+++ Express/app.js
@@ -0,0 +1,23 @@
+// Grabbing express module from the package
+const express = require('express');
+
+const app = express();
+const serveIndex = require('serve-index');
+
+
+// app.get('/', (req, res) => {
+//     res.send('Hello word');
+// });
+
+app.use('/nest',(req, res, next)=> {
+    // console.log("Time: ", Date.now());
+    console.log('Request type: ', req.method);
+    next();
+});
+
+app.use('/gators', express.static('public'));
+app.use('/gators', serveIndex('public'));
+
+app.listen(3000, ()=> {
+    console.log("Server listen on port 3000");
+});
